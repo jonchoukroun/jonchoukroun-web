e5c4b0d32e504533cb70eec4713cd56b14665020
Jon <jonchoukroun@gmail.com>
2019-01-06 22:58:05 -0800

    Mock up base project; limit to 1

--- app/components/project-card.ts
+++ app/components/project-card.ts
@@ -1,11 +1,18 @@
 import Component from '@ember/component';
 import { attribute } from '@ember-decorators/component';
+import { computed } from '@ember-decorators/object';
 
 export default class ProjectCardComponent extends Component {
 
-  project!: Object;
+  project!: any;
   priority!: Number;
 
   @attribute 'draggable' = true;
+
+  @computed('project.name')
+  get cardId() {
+    let arr: [String] = this.project.name.split(' ');
+    return `${arr.join('-')}-card`;
+  }
 
 }
